3a50cc9440af64f2739d314cfe88b88aed02177e
Qaiser <qaiser110@gmail.com>
2025-05-19 13:53:23 +1000

    SUPABASE_ANON_KEY

--- supabaseClient.js
+++ supabaseClient.js
@@ -1,6 +1,10 @@
 import "dotenv/config";
 import { createClient } from "@supabase/supabase-js";
 
+console.log({
+  SUPABASE_URL: process.env.SUPABASE_URL,
+  SUPABASE_ANON_KEY: process.env.SUPABASE_ANON_KEY,
+});
 export const supabase = createClient(
   process.env.SUPABASE_URL,
   process.env.SUPABASE_ANON_KEY
