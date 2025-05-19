d43897d42a9b91defe0084b61356bbd5749ffa37
Qaiser <qaiser110@gmail.com>
2025-05-19 14:00:49 +1000

    -

--- supabaseClient.js
+++ supabaseClient.js
@@ -2,5 +2,6 @@ import "dotenv/config";
 import { createClient } from "@supabase/supabase-js";
 
 const { SUPABASE_URL, SUPABASE_ANON_KEY } = process.env;
+console.log("SUPABASE_URL, SUPABASE_ANON_KEY", SUPABASE_URL, SUPABASE_ANON_KEY);
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
